ec5755645676330cade20f235ba8f3b611e27eee
kristinachan <10120193@cc.peralta.edu>
2022-04-04 15:45:55 -0700

    added shuffle to sort()
    also made all methods private except for one overloaded sort() method

--- src/ThreeWayQuicksort.java
+++ src/ThreeWayQuicksort.java
@@ -1,18 +1,24 @@
+import java.util.Random;
 import java.util.Scanner;
 
 final class ThreeWayQuicksort{
     private ThreeWayQuicksort(){}
 
+    public static void sort(Comparable[]a, int pivotChoice){
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1, pivotChoice);
+    }
     //recursive
-    public static void sort(Comparable[] a, int lo, int hi, int pivotChoice){
+    private static void sort(Comparable[] a, int lo, int hi, int pivotChoice){
         if (hi <= lo) return;   //base case: return if partitioned sub array has one element (it's sorted)
         int pivot = partition(a, lo, hi, pivotChoice);
+        //TODO need to switch to insertion sort once elements get less than 30 elements
         sort(a, lo, pivot - 1, pivotChoice); //sort left
         sort(a, pivot + 1, hi, pivotChoice); //sort right
     }
 
     //3-way partition, does the comparing of items
-    public static int partition(Comparable[] a, int lo, int hi, int pivotChoice){
+    private static int partition(Comparable[] a, int lo, int hi, int pivotChoice){
         int pivot = getPivot(a, lo, hi,pivotChoice);
         int leftIndex = lo;
         int rightIndex = hi + 1;
@@ -31,7 +37,7 @@ final class ThreeWayQuicksort{
         return rightIndex;
     }
 
-    public static int getPivot(Comparable[] a, int lo, int hi, int pivotChoice){
+    private static int getPivot(Comparable[] a, int lo, int hi, int pivotChoice){
         int pivot = 0;
         int median = lo + ((hi - lo)/2);
         //use switch case?
@@ -57,7 +63,7 @@ final class ThreeWayQuicksort{
         return pivot;
     }
 
-    public static Comparable[] insertionSort(Comparable[] a, int lo, int hi){
+    private static Comparable[] insertionSort(Comparable[] a, int lo, int hi){
         for(int i = 1; i < hi; i++){
             for(int j = i; j > 0 && (a[j].compareTo(a[j - 1]) < 0); j--){
                     swap(a, j, j - 1);
@@ -72,27 +78,40 @@ final class ThreeWayQuicksort{
         System.out.println(a[index1]+" swapped with "+a[index2]);
     }
 
-    //todo just made everything static was that a good idea I don't know?????
-
     public static void main(String[] args) {
-        String string = "ILOVEALGORITHMS";
-        Character[] array = new Character[15];
-        for (int i = 0; i < string.length(); i++) {
-            array[i] = string.charAt(i);
+        Random rng = new Random();
+        Double[] array1 = new Double[(int) Math.pow(10, 3)];
+        Double[] array2 = new Double[(int) Math.pow(10, 4)];
+        Double[] array3 = new Double[(int) Math.pow(10, 5)];
+        Double[] array4 = new Double[(int) Math.pow(10, 6)];
+
+        Double[][] arrays = new Double[4][];
+        arrays[0] = array1;
+        arrays[1] = array2;
+        arrays[3] = array3;
+        arrays[4] = array4;
+
+        for(int i = 0; i < 4; i++){
+
         }
 
-        int pivotChoice;
+        int[] pivots = new int[4];
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("For first element pivot, enter 1");
-        System.out.println("For median element pivot enter 2");
-        System.out.println("For left sub array median enter 3");
-        System.out.println("For right sub array median enter 4");
-        System.out.println("\n");
-        pivotChoice = keyboard.nextInt();
-
-        ThreeWayQuicksort.sort(array, 0, 14, pivotChoice);
-        for (int i = 0; i < string.length(); i++) {
-            System.out.println(array[i]);
+        for(int i = 0; i < 4; i++){
+            System.out.println("For first element pivot, enter 1");
+            System.out.println("For median element pivot enter 2");
+            System.out.println("For left sub array median enter 3");
+            System.out.println("For right sub array median enter 4");
+            pivots[i] = keyboard.nextInt();
         }
+
+        for(int j = 0; j < 4; j++){
+            long start = System.nanoTime();
+            ThreeWayQuicksort.sort(array1, pivots[j]);
+            long end = System.nanoTime();
+            System.out.println("Time to sort 10^" + (j+3) + "elements" + (end - start) + " nanoseconds");
+        }
+
+
     }
 }
