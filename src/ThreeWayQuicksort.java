222b4d5ab600a6cf4bb27b896c5eb544505163d2
kristinachan <10120193@cc.peralta.edu>
2022-04-03 17:52:34 -0700

    insertionSort() done
    wrote and fixed insertion sort method. Needed to compare a[j] to a[ j-1] not a[j] to a[i]

--- src/ThreeWayQuicksort.java
+++ src/ThreeWayQuicksort.java
@@ -2,29 +2,28 @@ public class ThreeWayQuicksort<Item extends Comparable>{
 
     //3-way partition, does the comparing of items
     private int partition(){
-
+        return 0;
     }
 
     //recursive
     public Item[] sort(Item[] a){
-
+        return a;
     }
 
     public int getPivot(Item[] array){
         //i. pivot is first item or any random item
         //ii. pivot is median of a[left], a[center], and a[right] of the sub array (or any three random elements in the sub array)
-        int pivot;
+        int pivot = 0;
         return pivot;
     }
 
     public Item[] insertionSort(Item[] a, int lo, int hi){
-        int length = hi - lo;
-        for(int i = 1; i < length; i++){
-            for(int j = i; j > 0; j--){
-                
+        for(int i = 1; i < hi; i++){
+            for(int j = i; j > 0 && (a[j].compareTo(a[j - 1]) < 0); j--){
+                    swap(a, j, j - 1);
             }
         }
-
+        return a;
     }
     private void swap(Item[] a, int index1, int index2){
         Item temp1 = a[index1];
@@ -36,9 +35,13 @@ public class ThreeWayQuicksort<Item extends Comparable>{
         ThreeWayQuicksort<Character> QS = new ThreeWayQuicksort<>();
         String string = "QUICKSORTEXAMPLE";
         Character[] array = new Character[16];
-        for(int i = 0; i < string.length(); i++){
-            array[0] = string.charAt(i);
+        for (int i = 0; i < string.length(); i++) {
+            array[i] = string.charAt(i);
+
         }
-        QS.insertionSort(array, 0, 15);
+        Character[] sortedArray = QS.insertionSort(array, 1, 16);
+        for (int i = 0; i < 16; i++) {
+            System.out.println(sortedArray[i]);
+        }
     }
 }
